fac1c2798cccd9177a74ac4e2d7b5a7a6bab6025
italo <italo@appbee.online>
2021-03-25 16:36:58 -0300

    Init Badges and Breadcrumbs

--- src/components/UIElements/Badges.vue
+++ src/components/UIElements/Badges.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>Badges</div>
+  <div class="min-h-screen w-full p-4">
+    <!--TITLE-->
+    <div class="section-title w-full mb-6 pt-3">
+      <div class="flex flex-row items-center justify-between mb-4">
+        <div class="flex flex-col">
+          <div class="text-xs uppercase font-light text-gray-500">UI Elements</div>
+          <div class="text-xl font-bold">Badges</div>
+        </div>
+      </div>
+    </div>
+
+    <section class="w-full p-4 mb-4 rounded-lg bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+      <div class="flex flex-row items-center justify-between mb-6">
+        <div class="flex flex-col">
+          <div class="text-sm font-light text-gray-500">Default badges</div>
+          <div class="text-sm font-bold">
+            <span>Use the <code>&lt;Badge /&gt;</code> component for simple badges. Use the <code>&lt;CircularBadge /&gt;</code> component for circular badges. Use the <code>rounded</code> prop for rounded badges.</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
--- src/components/UIElements/Breadcrumbs.vue
+++ src/components/UIElements/Breadcrumbs.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Breadcrumbs</div>
+  <!--TITLE-->
+  <div class="min-h-screen w-full p-4">
+    <!--TITLE-->
+    <div class="section-title w-full mb-6 pt-3">
+      <div class="flex flex-row items-center justify-between mb-4">
+        <div class="flex flex-col">
+          <div class="text-xs uppercase font-light text-gray-500">UI Elements</div>
+          <div class="text-xl font-bold">Breadcrumbs</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
