e6d7189aec0b32ef10f1d2937eb74fa1286ff381
italo <italo@appbee.online>
2021-03-26 13:25:45 -0300

    Badges completed

--- src/components/UIElements/Badges.vue
+++ src/components/UIElements/Badges.vue
@@ -9,7 +9,7 @@
         </div>
       </div>
     </div>
-
+    <!--BTN DEFAULT-->
     <section class="w-full p-4 mb-4 rounded-lg bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
       <div class="flex flex-row items-center justify-between mb-6">
         <div class="flex flex-col">
@@ -20,19 +20,73 @@
         </div>
       </div>
       <div class="flex flex-wrap items-center justify-start mb-2 w-full space-x-2">
-        <span class="uppercase font-bold text-white bg-blue-500 text-xs inline-flex text-center p-2">Default</span>
-        <span class="uppercase font-bold text-white bg-blue-500 text-xs inline-flex text-center p-1">Default</span>
-        <span class="uppercase font-bold text-white bg-blue-500 text-xs inline-flex text-center px-2">Default</span>
+        <span class="btn-primary p-2">Default</span>
+        <span class="btn-primary p-1">Default</span>
+        <span class="btn-primary px-2">Default</span>
       </div>
       <div class="flex flex-wrap items-center justify-start mb-2 w-full space-x-2">
-        <span class="uppercase font-bold text-white bg-blue-500 text-xs inline-flex text-center p-2 rounded-lg">Default</span>
-        <span class="uppercase font-bold text-white bg-blue-500 text-xs inline-flex text-center p-1 rounded-lg">Default</span>
-        <span class="uppercase font-bold text-white bg-blue-500 text-xs inline-flex text-center px-2 rounded-lg">Default</span>
+        <span class="btn-primary text-center rounded-lg p-2">Default</span>
+        <span class="btn-primary text-center rounded-lg p-1">Default</span>
+        <span class="btn-primary text-center rounded-lg px-2">Default</span>
       </div>
       <div class="flex flex-wrap items-center justify-start mb-2 w-full space-x-2">
-        <span class="uppercase font-bold text-white bg-blue-500 text-xs inline-flex text-xs p-0 h-6 w-6 rounded-full text-center justify-center">1º</span>
-        <span class="uppercase font-bold text-white bg-blue-500 text-xs inline-flex text-xs p-0 h-5 w-5 rounded-full text-center justify-center">1º</span>
-        <span class="uppercase font-bold text-white bg-blue-500 text-xs inline-flex text-xs p-0 h-4 w-4 rounded-full text-center justify-center">1º</span>
+        <span class="btn-primary rounded-full items-center justify-center p-0 h-7 w-7">1º</span>
+        <span class="btn-primary rounded-full items-center justify-center p-0 h-6 w-6">1º</span>
+        <span class="btn-primary rounded-full items-center justify-center p-0 h-5 w-5">1º</span>
+      </div>
+    </section>
+
+    <!--BTN OUTLINE-->
+    <section class="w-full p-4 mb-4 rounded-lg bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+      <div class="flex flex-row items-center justify-between mb-6">
+        <div class="flex flex-col">
+          <div class="text-sm font-light text-gray-500">Default badges</div>
+          <div class="text-sm font-bold">
+            <span>Use the <code>&lt;Badge /&gt;</code> component for simple badges. Use the <code>&lt;CircularBadge /&gt;</code> component for circular badges. Use the <code>rounded</code> prop for rounded badges.</span>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-wrap items-center justify-start mb-2 w-full space-x-2">
+        <span class="btn-primary-outline p-2">Default</span>
+        <span class="btn-primary-outline p-1">Default</span>
+        <span class="btn-primary-outline px-2">Default</span>
+      </div>
+      <div class="flex flex-wrap items-center justify-start mb-2 w-full space-x-2">
+        <span class="btn-primary-outline text-center rounded-lg p-2">Default</span>
+        <span class="btn-primary-outline text-center rounded-lg p-1">Default</span>
+        <span class="btn-primary-outline text-center rounded-lg px-2">Default</span>
+      </div>
+      <div class="flex flex-wrap items-center justify-start mb-2 w-full space-x-2">
+        <span class="btn-primary-outline rounded-full items-center justify-center p-0 h-7 w-7">1º</span>
+        <span class="btn-primary-outline rounded-full items-center justify-center p-0 h-6 w-6">1º</span>
+        <span class="btn-primary-outline rounded-full items-center justify-center p-0 h-5 w-5">1º</span>
+      </div>
+    </section>
+
+    <!--BTN Alternative colors-->
+    <section class="w-full p-4 mb-4 rounded-lg bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+      <div class="flex flex-row items-center justify-between mb-6">
+        <div class="flex flex-col">
+          <div class="text-sm font-light text-gray-500">Alternative colors</div>
+          <div class="text-sm font-bold">
+            <span>Use the <code>alt</code> prop for badges with alternative colors.</span>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-wrap items-center justify-start mb-2 w-full space-x-2">
+        <span class="btn-alternative p-2">Default</span>
+        <span class="btn-alternative p-1">Default</span>
+        <span class="btn-alternative px-2">Default</span>
+      </div>
+      <div class="flex flex-wrap items-center justify-start mb-2 w-full space-x-2">
+        <span class="btn-alternative text-center rounded-lg p-2">Default</span>
+        <span class="btn-alternative text-center rounded-lg p-1">Default</span>
+        <span class="btn-alternative text-center rounded-lg px-2">Default</span>
+      </div>
+      <div class="flex flex-wrap items-center justify-start mb-2 w-full space-x-2">
+        <span class="btn-alternative rounded-full items-center justify-center p-0 h-7 w-7">1º</span>
+        <span class="btn-alternative rounded-full items-center justify-center p-0 h-6 w-6">1º</span>
+        <span class="btn-alternative rounded-full items-center justify-center p-0 h-5 w-5">1º</span>
       </div>
     </section>
   </div>
