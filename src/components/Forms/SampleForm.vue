ad6962f0f7418ddc328c2d858e826032a4233227
italo <italo@appbee.online>
2021-04-07 13:14:25 -0300

    Finish component SampleForm

--- src/components/Forms/SampleForm.vue
+++ src/components/Forms/SampleForm.vue
@@ -33,19 +33,52 @@
                 <!--Email-->
                 <div class="flex flex-col mb-4">
                   <div class="mb-2">Email</div>
-                  <input name="name" type="text" class="block text-sm text-gray-500 leading-none w-full px-2 py-3 border border-gray-500 rounded focus:outline-none" placeholder="Enter you email">
+                  <input name="name" type="email" class="block text-sm text-gray-500 leading-none w-full px-2 py-3 border border-gray-500 rounded focus:outline-none" placeholder="Enter you email">
                 </div>
                 <!--Message-->
                 <div class="flex flex-col mb-4">
                   <div class="mb-2">Message</div>
 
                   <textarea class="block text-sm text-gray-500 leading-none w-full px-2 py-3 border border-gray-500 rounded focus:outline-none"
-                            name="" id="" cols="30" rows="4" placeholder="Enter you message...">
+                            cols="30" rows="4" placeholder="Enter you message...">
 
                   </textarea>
                 </div>
               </div>
-              <button class="bg-blue-400 text-gray-50 px-3 py-2 rounded-md focus:outline-none">Enviar</button>
+              <input type="submit" class="btn btn-default bg-blue-500 hover:bg-blue-600 text-white rounded-md cursor-pointer focus:outline-none">
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!--Login-->
+    <section class="w-full p-4 mb-4 rounded-lg bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+      <div class="flex flex-row items-center justify-between mb-6">
+        <div class="flex flex-col">
+          <div class="text-sm font-light text-gray-500">Login</div>
+          <div class="text-sm font-bold">
+            <span>Sample login form</span>
+          </div>
+        </div>
+      </div>
+      <div class="w-full flex">
+        <div class="w-full lg:w-1/2">
+          <div class="flex flex-col">
+            <form action="" class="form flex flex-wrap w-full">
+              <div class="w-full">
+                <!--Email-->
+                <div class="flex flex-col mb-4">
+                  <div class="mb-2">Email</div>
+                  <input name="name" type="Email" class="block text-sm text-gray-500 leading-none w-full px-2 py-3 border border-gray-500 rounded focus:outline-none" placeholder="Enter you email">
+                </div>
+                <!--Password-->
+                <div class="flex flex-col mb-4">
+                  <div class="mb-2">Password</div>
+                  <input name="name" type="password" class="block text-sm text-gray-500 leading-none w-full px-2 py-3 border border-gray-500 rounded focus:outline-none" placeholder="Enter you email">
+                </div>
+              </div>
+              <input type="submit" class="btn btn-default bg-blue-500 hover:bg-blue-600 text-white rounded-md cursor-pointer focus:outline-none">
             </form>
           </div>
         </div>
